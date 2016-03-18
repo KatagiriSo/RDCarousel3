61826b51d08b949d7431286ea88288c2693c125e
Katagiri <katagiri@plala.co.jp>
2016-03-18 14:57:09 +0900

    add License

--- RDCarousel3/ViewController.h
+++ RDCarousel3/ViewController.h
@@ -3,8 +3,9 @@
 //  RDCarousel3
 //
 //  Created by 片桐奏羽 on 2016/03/17.
-//  Copyright © 2016年 SoKatagiri. All rights reserved.
-//
+//  Copyright © 2016年 SoKatagiri. 
+//  This software is released under the MIT License.
+//   http://opensource.org/licenses/mit-license.php
 
 #import <UIKit/UIKit.h>
 
